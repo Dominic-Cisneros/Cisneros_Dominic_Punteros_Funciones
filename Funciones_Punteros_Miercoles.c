795d7af053f3f01a9a265570fbc2e52ccf31100e
Dominic <dominic.cisneros@udla.edu.ec>
2026-05-20 15:04:01 -0500

    Funcion calcular promedio por valor

--- Funciones_Punteros_Miercoles.c
+++ Funciones_Punteros_Miercoles.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+float calcularPromedio(float notas[5]);
+
 int main(int argc, char *argv[]) {
 
     float notas[5] = {6.5, 7.1, 8, 6, 5};
+
+    printf("promedio:%.2f\n", calcularPromedio(notas));
+
+    return 0;
+}
+
+float calcularPromedio(float notas[5]) {
+
     float suma = 0;
 
     for (int i = 0; i < 5; i++) {
@@ -10,9 +20,7 @@ int main(int argc, char *argv[]) {
         suma += notas[i];
     }
 
-    float promedio = suma / 5.0;
+    float promedio = suma / 5.0f;
 
-    printf("promedio: %.2f\n", promedio);
-
-    return 0;
+    return promedio;
 }
